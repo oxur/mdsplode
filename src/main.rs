614f509327c8dc232fea5a68b9ad2548d0076fca
Duncan McGreggor <oubiwann@gmail.com>
2023-12-26 22:49:59 -0600

    More clap tweaking.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long)]
-    file: Option<String>,
+    #[arg(short, long, required = true, help = "Input file or directory")]
+    input: String,
+    #[arg(short, long, default_value = ".", help = "Output file or directory")]
+    output: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
-    let mut tree = mdsplode::parse_file(cli.file.unwrap().as_str());
+    let mut tree = mdsplode::parse_file(cli.input.as_str());
     tree.traverse();
     println!("{:}", tree.to_json());
 }
