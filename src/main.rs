b30b091fa12061f381a549f01f91e881f868d0e8
Duncan McGreggor <oubiwann@gmail.com>
2023-12-26 19:45:12 -0600

    Added clap and --file support.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    file: Option<String>,
+}
+
 fn main() {
-    let file = "./workbench/learn.md";
-    let mut tree = mdsplode::parse_file(file);
+    let cli = Cli::parse();
+    let mut tree = mdsplode::parse_file(cli.file.unwrap().as_str());
     tree.traverse();
     println!("{:}", tree.to_json());
 }
