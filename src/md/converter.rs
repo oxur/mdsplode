c6c22b0d4f911f1c91a206d9a853f91ffdcc37b7
Duncan McGreggor <oubiwann@gmail.com>
2023-12-28 17:37:17 -0600

    Refactored code for better/more usable payload.

--- src/md/converter.rs
+++ src/md/converter.rs
@@ -4,13 +4,15 @@ use markdown::mdast::Node;
 
 use super::opts;
 
-pub fn node_to_html(node: Node) -> String {
+pub fn markdown_to_html(node: Node) -> String {
     markdown::to_html_with_options(node.to_string().as_str(), &opts::options()).unwrap()
 }
 
-pub fn node_to_toml(node: Node) -> String {
+pub fn toml_to_json(node: Node) -> String {
     let table = node.to_string().as_str().parse::<toml::Table>().unwrap();
-    serde_json::to_string_pretty(&table).unwrap()
+    let mut wrapper = toml::Table::new();
+    wrapper.insert("frontmatter".to_string(), toml::Value::Table(table.clone()));
+    serde_json::to_string(&wrapper).unwrap()
 }
 
 pub fn file_to_mdast(file: &str) -> Node {
