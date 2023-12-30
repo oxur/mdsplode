65b7cefa4cfa05bea7a397ed55975c0a8762c07e
Duncan McGreggor <oubiwann@gmail.com>
2023-12-30 13:25:54 -0600

    Added integration tests.

---
+++ tests/common.sh
@@ -0,0 +1,13 @@
+BIN=bin/mdsplode
+export RUST_BACKTRACE=1
+TMP_DIR=/tmp/$(date +"%Y%m%d.%H%M%S")/mdsplode
+MD_FILE=./tests/data/learn.md
+
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+END_COLOR="\033[0m"
+
+function header () {
+    echo -e "${GREEN}>>${END_COLOR} ${YELLOW}${1}${END_COLOR}"
+    echo
+}
---
+++ tests/mdsplode_dev.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script is for integration tests of unreleased versions of rucksack
+# that are still in-development.
+
+make build
+
+. ./tests/common.sh || . ./common.sh
+
+echo
+header "Show top-level help"
+echo
+./$BIN --help
+echo
+
+header "View entire parsed data set"
+./$BIN --input "$MD_FILE" --pretty
+echo
+
+header "View filtered subset"
+./$BIN --input "$MD_FILE" --pretty \
+--query '.children.nodes[] | select(((.depth == 3) and .name == "heading") and .source == "Getting Started")'
+echo
+
+header "View HTML for all headings of depth 3"
+./$BIN --input "$MD_FILE" \
+--query '.children.nodes[] | select((.depth == 3) and .name == "heading") | .children.nodes[].html'
+echo
+
+header "View the parsed front matter for the Markdowwn file"
+./$BIN --input "$MD_FILE" \
+--query '.children.nodes[] | select(.name == "toml") | .json' | \
+jq -r . | jq .frontmatter
+echo
