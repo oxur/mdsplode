1d206bf14ff6cd1988ffca1a171dbca5cfc46d3b
Duncan McGreggor <oubiwann@gmail.com>
2023-12-30 21:30:03 -0600

    Added support for skip-processing.

--- src/sploder/file.rs
+++ src/sploder/file.rs
@@ -1,5 +1,5 @@
-use std::io;
 use std::io::prelude::*;
+use std::{fs, io};
 
 use anyhow::{anyhow, Error, Result};
 
@@ -8,10 +8,16 @@ use crate::parse_file;
 
 use super::util::{pretty_print, run_query};
 
-pub fn process(in_file: String, _out_file: String, cli: Opts) -> Result<(), Error> {
-    // --input - already taken care of in validation
-    let tree = parse_file(in_file.as_str());
-    let mut json = tree.to_json();
+pub fn process(in_file: String, out_file: String, cli: Opts) -> Result<(), Error> {
+    let mut json: String;
+    // --skip-process
+    if cli.skip_process {
+        json = read_to_string(in_file)?;
+    } else {
+        // --input - already taken care of in validation
+        let tree = parse_file(in_file.as_str());
+        json = tree.to_json();
+    }
     // --query
     if let Some(ref query) = cli.query {
         if let Ok(result) = run_query(json.clone(), query.to_string()) {
@@ -20,18 +26,28 @@ pub fn process(in_file: String, _out_file: String, cli: Opts) -> Result<(), Erro
     };
     // --pretty
     if cli.pretty {
-        json = pretty_print(json)?
+        json = pretty_print(json.clone())?
     }
     // --output
     if cli.is_stdout() {
         let stdout = io::stdout();
         let mut stdout = stdout.lock();
-        match writeln!(stdout, "{:}", json) {
+        match writeln!(stdout, "{}", json.clone()) {
             Err(e) => Err(anyhow!("{:}", e)),
             _ => Ok(()),
         }
     } else {
-        // TODO: create a file for writing, write to it
-        unimplemented!()
+        let mut out = fs::File::create(out_file)?;
+        match write!(out, "{}", json) {
+            Err(e) => Err(anyhow!("{:}", e)),
+            _ => Ok(()),
+        }
+    }
+}
+
+fn read_to_string(filename: String) -> Result<String, Error> {
+    match fs::read_to_string(filename.as_str()) {
+        Err(e) => return Err(anyhow!("{:}", e)),
+        Ok(data) => Ok(data),
     }
 }
