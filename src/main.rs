ae798b4af27542ee946217d9ef8d77fc34fabc4e
Duncan McGreggor <oubiwann@gmail.com>
2023-12-31 11:17:22 -0600

    Implemented dir-walking processor.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use mdsplode::sploder::{dir, file};
 fn main() -> Result<(), Error> {
     reset_sigpipe();
     let cli = Opts::parse();
-    cli.validate()?;
+    cli.validate()?; // This ensures that when is_dir, input & output are dirs
     let input = cli.input.clone();
     let output = cli.output.clone();
     if cli.is_dir() {
--- src/sploder/dir.rs
+++ src/sploder/dir.rs
@@ -1,7 +1,27 @@
+use std::path;
+
 use anyhow::{Error, Result};
+use walkdir::WalkDir;
 
 use crate::cli::opts::Opts;
 
-pub fn process(_in_path: String, _out_path: String, _cli: Opts) -> Result<(), Error> {
+use super::file;
+
+pub fn process(in_path: String, out_path: String, cli: Opts) -> Result<(), Error> {
+    for entry in WalkDir::new(in_path.clone())
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let in_segs = entry
+            .path()
+            .iter()
+            .map(|x| x.to_str().unwrap())
+            .collect::<Vec<_>>();
+        let tail: Vec<&str> = in_segs[1..].to_vec();
+        let head: Vec<&str> = vec![out_path.as_str()];
+        let out_segs = [head, tail].concat();
+        let out_file = out_segs.join(path::MAIN_SEPARATOR_STR).to_string();
+        file::process(in_path.to_string(), out_file, cli.clone())?;
+    }
     Ok(())
 }
