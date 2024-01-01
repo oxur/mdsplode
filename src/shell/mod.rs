38158870886b9410ef36d6dcbbf781991e6134ac
Duncan McGreggor <oubiwann@gmail.com>
2024-01-01 14:29:35 -0600

    Added colour to banner.

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -4,6 +4,7 @@ pub mod state;
 pub mod writer;
 
 use anyhow::{Error, Result};
+use const_format::formatcp;
 
 use crate::cli::opts::Opts;
 
@@ -22,7 +23,7 @@ fn shell(mut state: State) -> Result<State, String> {
         log::debug!("Got state: {:?}", state);
         if state.show_banner {
             state.show_banner = false;
-            writer::msg(state.clone(), BANNER)?;
+            writer::msg(state.clone(), banner())?;
             continue;
         }
         let line = reader::line(state.clone())?;
@@ -46,17 +47,29 @@ fn shell(mut state: State) -> Result<State, String> {
     Ok(state)
 }
 
-const BANNER: &str = r#"
+fn banner() -> &'static str {
+    const RED: &str = "\x1B[1;31m";
+    const YELLOW_BOLD: &str = "\x1B[1;33m";
+    const YELLOW: &str = "\x1B[33m";
+    const GREEN: &str = "\x1B[32m";
+    const BLUE: &str = "\x1B[34m";
+    const WHITE: &str = "\x1B[1;37m";
+
+    const END: &str = "\x1B[0m";
+    formatcp!(
+        r#"
 Welcome to
 
-      ,--.!,
-   __/   -*-     _____ ____  _       ___   ___      ___
- ,d08b.  '|`    / ___/|    \| T     /   \ |   \    /  _]
- 0088MM        (   \_ |  o  ) |    Y     Y|    \  /  [_
- `9MMP'      __ \__  T|   _/| l___ |  O  ||  D  YY    _]
-.--------.--|  |/  \ ||  |  |     T|     ||     ||   [_
+        {WHITE},--{END}{RED}.{END}{YELLOW}!{END}{RED},{END}
+     {BLUE}__{END}{WHITE}/{END}   {YELLOW}-{END}{YELLOW_BOLD}*{END}{YELLOW}-{END}   {GREEN}_____ ____  _       ___   ___      ___{END}
+   {BLUE},d08b.{END}  {RED}'{END}{YELLOW}|{END}{RED}`{END}  {GREEN}/ ___/|    \| T     /   \ |   \    /  _]{END}
+   {BLUE}0088MM{END}      {GREEN}(   \_ |  o  ) |    Y     Y|    \  /  [_{END}
+   {BLUE}`9MMP'{END}    __ {GREEN}\__  T|   _/| l___ |  O  ||  D  YY    _]
+{GREEN}.--------.--|  |/  \ ||  |  |     T|     ||     ||   [_
 |        |  _  |\    ||  |  |     |l     !|     ||     T
-|__|__|__|_____| \___jl__j  l_____j \___/ l_____jl_____j
+|__|__|__|_____| \___jl__j  l_____j \___/ l_____jl_____j{END}
 
 To see a list of available commands, type "help" at the prompt.
-"#;
+"#
+    )
+}
