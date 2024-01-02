63c606033d92fe558d8946226204cc89e31a1951
Duncan McGreggor <oubiwann@gmail.com>
2024-01-01 20:57:15 -0600

    Cleaned up the API for reading files.

--- src/sploder/file.rs
+++ src/sploder/file.rs
@@ -2,10 +2,13 @@ use std::io::prelude::*;
 use std::{fs, io};
 
 use anyhow::{anyhow, Error, Result};
+use markdown::mdast::Node;
 
 use crate::cli::opts::Opts;
-use crate::parse_file;
+use crate::md::converter;
 
+use super::parser;
+use super::types::CompoundNode;
 use super::util::{pretty_print, run_query};
 
 pub fn process(in_file: String, out_file: String, cli: Opts) -> Result<(), Error> {
@@ -16,7 +19,7 @@ pub fn process(in_file: String, out_file: String, cli: Opts) -> Result<(), Error
         json = read_to_string(in_file)?;
     } else {
         // --input - already taken care of in validation
-        let tree = parse_file(in_file.as_str());
+        let tree = read_to_parsed(in_file)?;
         json = tree.to_json();
     }
     // --query
@@ -46,9 +49,19 @@ pub fn process(in_file: String, out_file: String, cli: Opts) -> Result<(), Error
     }
 }
 
-fn read_to_string(filename: String) -> Result<String, Error> {
+pub fn read_to_string(filename: String) -> Result<String, Error> {
     match fs::read_to_string(filename.as_str()) {
         Err(e) => Err(anyhow!("{:}", e)),
         Ok(data) => Ok(data),
     }
+}
+
+pub fn read_to_mdast(filename: String) -> Result<Node, Error> {
+    let md = read_to_string(filename)?;
+    Ok(converter::string_to_mdast(md))
+}
+
+pub fn read_to_parsed(filename: String) -> Result<CompoundNode, Error> {
+    let node = read_to_mdast(filename)?;
+    Ok(parser::parse_node(&node))
 }
