c5f2172ec2246faab10978247d15f0bfa19b73a2
Duncan McGreggor <oubiwann@gmail.com>
2024-01-04 02:05:25 -0600

    Updated to use the latest twyg.

--- src/logging.rs
+++ src/logging.rs
@@ -1,17 +1,17 @@
 use anyhow::{anyhow, Error, Result};
+use twyg::{self, out};
 
 use crate::cli::opts::Opts;
 
 pub fn setup(opts: Opts) -> Result<(), Error> {
     let cfg = twyg::Opts {
         coloured: !opts.no_colour,
-        file: Some("stderr".to_string()),
+        file: out::stderr(),
         level: Some(opts.log_level),
         report_caller: true,
 
         ..Default::default()
     };
-    owo_colors::set_override(cfg.coloured);
 
     let result = match twyg::setup(cfg.clone()) {
         Err(e) => Err(anyhow!("{:}", e)),
