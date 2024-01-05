7e4b5752a7e2a60eaf953b26c58ce70620c7f2ec
Duncan McGreggor <oubiwann@gmail.com>
2024-01-04 21:35:23 -0600

    Removed now-unncessary etx ending support.
    (Due to rustyline addressing the issue ETX support intendeed to solve.)

--- src/shell/state.rs
+++ src/shell/state.rs
@@ -14,7 +14,6 @@ pub struct State {
     pub result: String,
     pub show_banner: bool,
     pub source: String,
-    pub with_ext_line_ending: bool,
     pub without_colour: bool,
 }
 
@@ -25,7 +24,6 @@ impl State {
             history_size: opts.history_size,
             prompt: opts.prompt,
             show_banner: !opts.no_banner,
-            with_ext_line_ending: opts.etx_line_ending,
             without_colour: opts.no_colour,
             ..State::default()
         }
